1069f4d49df1859dff3bebb1cc0cc1e57c985687
Tanya <tanya.naidenova@abv.bg>
2023-09-18 22:22:35 +0300

    Added structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './core/authentication/login/login.component';
+import { SignupComponent } from './core/authentication/signup/signup.component';
+import { RestorePasswordComponent } from './core/authentication/restore-password/restore-password.component';
+import { FooterComponent } from './core/ui/footer/footer.component';
+import { HeaderComponent } from './core/ui/header/header.component';
+import { HomeComponent } from './core/ui/home/home.component';
+import { LoaderComponent } from './core/ui/loader/loader.component';
+import { MenuComponent } from './core/ui/menu/menu.component';
+import { NotFoundComponent } from './core/ui/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    RestorePasswordComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoaderComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
