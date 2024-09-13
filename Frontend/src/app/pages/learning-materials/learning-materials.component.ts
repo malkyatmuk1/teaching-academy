ee1a667364aaab3fff568303ce757783d0706a33
psabev <psabev@lirex.com>
2024-09-14 00:11:33 +0300

    Optimizations

--- Frontend/src/app/pages/learning-materials/learning-materials.component.ts
+++ Frontend/src/app/pages/learning-materials/learning-materials.component.ts
@@ -11,69 +11,90 @@ export class LearningMaterialsComponent {
     {
       key: '0',
       label: 'Ученици',
-      data: 'Students',
+      data: null,
       icon: 'pi pi-fw pi-inbox',
       children: [
         {
-          key: '0-0',
-          label: '1-ви клас',
-          data: 'First grade',
+          key: '1-4',
+          label: '1-4 клас',
+          data: null,
           icon: 'pi pi-fw pi-tag',
           children: [
-            { key: '0-0-0', label: 'Събиране.docx', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
-            { key: '0-0-1', label: 'Таблица за умножение.docx', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+            { key: '1-4-1', label: 'Формули.pdf', icon: 'pi pi-fw pi-file', data: 'assets/learning-materials/1-4-grade/Формули-Експонента.pdf' },
           ]
         },
         {
-          key: '0-1',
-          label: '2-ри клас',
-          data: 'Second grade',
+          key: '5-6',
+          label: '5-6 клас',
+          data: null,
           icon: 'pi pi-fw pi-tag',
-          children: [{ key: '0-1-0', label: 'Геометрия.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
-        }
+          children: [
+            { key: '5-6-1', label: 'Формули.pdf', icon: 'pi pi-fw pi-file', data: 'assets/learning-materials/5-6-grade/Формули-Експонента.pdf' },
+          ]
+        },
+        {
+          key: '7',
+          label: '7 клас',
+          data: null,
+          icon: 'pi pi-fw pi-tag',
+          children: [
+            { key: '7-0-1', label: 'Формули.pdf', icon: 'pi pi-fw pi-file', data: 'assets/learning-materials/7-grade/Формули-Експонента.pdf' },
+          ]
+        },
       ]
     },
     {
       key: '0',
       label: 'Студенти',
-      data: 'Uni_Students',
+      data: null,
       icon: 'pi pi-fw pi-inbox',
       children: [
-        {
-          key: '0-0',
-          label: '1-ви курс',
-          data: 'First course',
-          icon: 'pi pi-fw pi-tag',
-          children: [
-            { key: '0-0-0', label: 'Събиране.docx', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
-            { key: '0-0-1', label: 'Таблица за умножение.docx', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
-          ]
-        },
-        {
-          key: '0-1',
-          label: '2-ри курс',
-          data: 'Second course',
-          icon: 'pi pi-fw pi-tag',
-          children: [{ key: '0-1-0', label: 'Геометрия.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
-        },
-        {
-          key: '0-2',
-          label: '3-ти курс',
-          data: 'Third course',
-          icon: 'pi pi-fw pi-tag',
-          children: [
-            { key: '0-0-0', label: 'Събиране.docx', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
-            { key: '0-0-1', label: 'Таблица за умножение.docx', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
-          ]
-        },
-        {
-          key: '0-3',
-          label: '4-ти курс',
-          data: 'Fourth course',
-          icon: 'pi pi-fw pi-tag',
-          children: [{ key: '0-1-0', label: 'Геометрия.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
-        }
+        // {
+        //   key: '0-0',
+        //   label: '1-ви курс',
+        //   data: 'First course',
+        //   icon: 'pi pi-fw pi-tag',
+        //   children: [
+        //     { key: '0-0-0', label: 'Събиране.docx', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+        //     { key: '0-0-1', label: 'Таблица за умножение.docx', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+        //   ]
+        // },
+        // {
+        //   key: '0-1',
+        //   label: '2-ри курс',
+        //   data: 'Second course',
+        //   icon: 'pi pi-fw pi-tag',
+        //   children: [{ key: '0-1-0', label: 'Геометрия.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
+        // },
+        // {
+        //   key: '0-2',
+        //   label: '3-ти курс',
+        //   data: 'Third course',
+        //   icon: 'pi pi-fw pi-tag',
+        //   children: [
+        //     { key: '0-0-0', label: 'Събиране.docx', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+        //     { key: '0-0-1', label: 'Таблица за умножение.docx', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+        //   ]
+        // },
+        // {
+        //   key: '0-3',
+        //   label: '4-ти курс',
+        //   data: 'Fourth course',
+        //   icon: 'pi pi-fw pi-tag',
+        //   children: [{ key: '0-1-0', label: 'Геометрия.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
+        // }
       ]
     },
   ];
+
+  onFileSelect(e: any) {
+    if (e?.node?.data) {
+      const filePath = e?.node?.data;
+      const link = document.createElement('a');
+      link.href = filePath;
+      link.download = filePath.split('/').pop();
+      link.click();
+    }
+
+  }
 }
